1592cf7260714dd6244557b3534f4ce04cb4fcb3
JuanJFreydell <juanjfreydell@gmail.com>
2025-01-28 18:37:55 -0800

    making the site responsive

---
+++ app/(variables)/teams.ts
@@ -0,0 +1,29 @@
+export const teams = [
+  {
+    name: "Rendi",
+    projectDescription: "Building a real-time chat app with secure messaging.",
+    stack: ["React", "Node.js", "Socket.IO", "MongoDB"],
+    presentationDate: "2025-02-15",
+    members: [
+      {
+        name: "Juan Jose Freydell",
+        picture:
+          "https://media.licdn.com/dms/image/v2/D5603AQHt2ToowPPwkw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1675456912927?e=1743638400&v=beta&t=L4HQjS5fF7V01eWIj-Hm0cgBEfzEPzeeRbsisQ4v0NA",
+      },
+    ],
+  },
+  {
+    name: "Zip-a-Trip",
+    projectDescription:
+      "Enabling uses to find the most efficicent way to plan multi-legged trips.",
+    stack: ["React", "Node", "Google Maps", "AWS"],
+    presentationDate: "2025-02-15",
+    members: [
+      {
+        name: "Yi-Chen Chao",
+        picture:
+          "https://media.licdn.com/dms/image/v2/D5635AQGnlIZ3H2TwJQ/profile-framedphoto-shrink_800_800/profile-framedphoto-shrink_800_800/0/1726383945968?e=1738720800&v=beta&t=d23kTtjs8OdzVMZGXPHwb9R-z8N6ioN6xb-u0AEVO8A",
+      },
+    ],
+  },
+];
--- app/layout.tsx
+++ app/layout.tsx
@@ -29,21 +29,24 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {/* Navigation Bar */}
-        <nav className="flex h-14 w-full justify-between items-center fixed top-0 right-0 bg-black z-50">
-          <div className="h-full flex flex-row">
+        <nav className="flex h-14 w-full items-center fixed top-0 right-0 bg-black z-50">
+          <div className="h-full w-full flex flex-row">
             <p className="h-full flex bg-red-600 w-fit items-center text-white px-5 text-3xl font-serif">
               N
             </p>
-            <p className="h-full flex items-center text-white px-10 text-3xl font-serif">
+            <p className="h-full hidden sm:flex items-center text-white px-10 text-3xl font-serif">
               App Lab NU
             </p>
           </div>
-          <ul className="flex h-full items-center justify-between gap-8 flex-row text-white text-xl pr-10">
-            <Link href="/" className="cursor-pointer hover:text-red-600">
+          <ul className="px-2 flex w-full justify-center h-full items-center gap-4 flex-row text-white text-xl">
+            <Link href="/about" className="cursor-pointer hover:text-red-600">
               About
             </Link>
             <Link href="/teams" className="cursor-pointer hover:text-red-600">
-              Active Teams
+              Groups
+            </Link>
+            <Link href="/members" className="cursor-pointer hover:text-red-600">
+              Members
             </Link>
           </ul>
         </nav>
