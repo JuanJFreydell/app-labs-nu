7f1b8c7a424503c2b6e277ee7cc22a5262c8e56a
Dylan Ullrich <dylan@eldyl.org>
2025-04-02 09:52:22 -0700

    Enhancement: teams pager uses "marketing" shell component

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -1,28 +1,30 @@
 import React from "react";
 import TeamCard from "@/components/teamsCard";
+import MarketingPage from "@/components/marketing-page";
 import { teams } from "@/variables/teams";
 
 export default function TeamsList() {
+  const pageTitle = "Active Teams";
+  const pageDescription =
+    "Here are some of the projects that members of App Lab Northeastern are currently working on.";
   return (
-    <div className="min-h-screen bg-gray-100">
-      {/* Title */}
-      <h1 className="mb-10 pt-20 text-center text-4xl font-bold text-black">
-        Active Teams
-      </h1>
-
+    <MarketingPage pageTitle={pageTitle} pageDescription={pageDescription}>
       {/* Team Cards */}
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <ul
+        role="list"
+        className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8"
+      >
         {teams.map((team, index) => (
-          <div key={index} className="p-4">
+          <li key={index} className="p-4">
             <TeamCard
               team={{
                 ...team,
                 members: team.members,
               }}
             />
-          </div>
+          </li>
         ))}
-      </div>
-    </div>
+      </ul>
+    </MarketingPage>
   );
 }
