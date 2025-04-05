e3eed634dfa545bfe969d03b591b6ad5931f5fa7
Dylan Ullrich <dylan@eldyl.org>
2025-04-04 21:47:47 -0700

    Polish zod schemas for login

--- src/types/zod/login-form.ts
+++ src/types/zod/login-form.ts
@@ -1,18 +1,21 @@
 import { z } from "zod";
 
-// Create a schema for form validation
-export const loginSchema = z.object({
+export const emailSchema = z.object({
   email: z
     .string()
     .email("Please enter a valid email address")
     .refine(
       (email) => email.endsWith("@northeastern.edu"),
-      "Email must be a Northeastern University email address (@northeastern.edu)"
+      "Email must be a Northeastern University email address (@northeastern.edu)",
     ),
-  password: z
-    .string()
-    .min(8, "Password must be at least 8 characters")
 });
 
-// Type for the form data
-export type LoginFormData = z.infer<typeof loginSchema>;
+export const otpSchema = z.object({
+  otp: z
+    .string()
+    .min(6, "OTP must be 6 digits long")
+    .refine((value) => /^\d*$/.test(value), "OTP code is numbers only"),
+});
+
+export type EmailFormData = z.infer<typeof emailSchema>;
+export type OtpSchema = z.infer<typeof otpSchema>;
