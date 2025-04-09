cb0044b81128ef9ef4a1e1680e0e669a648ce850
Dylan Ullrich <dylan@eldyl.org>
2025-04-09 13:10:34 -0700

    Create zod schema for user settings form

--- src/zod/user.ts
+++ src/zod/user.ts
@@ -1,11 +1,44 @@
 import { z } from "zod";
 
-export const basicUserInfo = z.object({
+export const basicUserInfoSchema = z.object({
   firstName: z.string().nullable(),
   lastName: z.string().nullable(),
   email: z.string(),
-  avatarUrl: z.string().url().nullable(),
+  avatarUrl: z.string().url().or(z.string().max(0)).nullable(),
   isProfileComplete: z.boolean(),
 });
 
-export type BasicUserInfo = z.infer<typeof basicUserInfo>;
+export type BasicUserInfo = z.infer<typeof basicUserInfoSchema>;
+
+export const userSettingsFormSchema = z.object({
+  firstName: z.string().max(100, "Must be 100 characters or less"),
+  lastName: z.string().max(100, "Must be 100 characters or less"),
+  email: z
+    .string()
+    .email("Please enter a valid email address")
+    .refine(
+      (email) => email.endsWith("@northeastern.edu"),
+      "Email must be a Northeastern University email address (@northeastern.edu)",
+    ),
+  avatarUrl: z.string().url().or(z.string().max(0)),
+  jobTitle: z.string().max(100, "Must be 100 characters or less"),
+  linkedinUrl: z.string().url().or(z.string().max(0)),
+  githubUrl: z.string().url().or(z.string().max(0)),
+  interests: z
+    .string()
+    .min(1, "Must be at least 1 character")
+    .max(100, "Must be 100 characters or less")
+    .array()
+    .max(8, "You can only list 8 interests"),
+  skills: z
+    .string()
+    .min(1, "Must be at least 1 character")
+    .max(100, "Must be 100 characters or less")
+    .array()
+    .max(8, "You can only list 8 skills"),
+  isProfileComplete: z.boolean(),
+  // preferences: z.undefined(),
+  isProfilePublic: z.boolean(),
+});
+
+export type UserSettingsForm = z.infer<typeof userSettingsFormSchema>;
