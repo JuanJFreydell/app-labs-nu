f624a99d6ac7c908d20beb22422fd76b18cca94e
Dylan Ullrich <dylan@eldyl.org>
2025-04-09 13:15:12 -0700

    update handler exports

--- src/db/handlers/user/index.ts
+++ src/db/handlers/user/index.ts
@@ -1,2 +1,3 @@
 export * from "@/db/handlers/user/get-authenticated-user-profile";
+export * from "@/db/handlers/user/update-user-profile";
 export * from "@/db/handlers/user/get-authenticated-user-profile-basic";
